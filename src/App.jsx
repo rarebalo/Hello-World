c6de6fd4387308965524c40475b337fbe2fe10f7
Roberto <roberto.v.arebalo@gmail.com>
2024-01-01 20:31:48 -0300

    le agrego estilo al titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css' 
 function App() {
-  return <h1>Hello Worl</h1>;
+  return <h1 className='text-center display-3'>Hello World</h1>;
 }
 export default App;
